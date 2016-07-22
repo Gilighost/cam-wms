a0cbfa7461dcd943547ac83632a3e410c4c00524
Cameron <claferne@harding.edu>
2016-07-22 11:45:48 -0400

    Undid test changes

--- app/controllers/getMapHandler.js
+++ app/controllers/getMapHandler.js
@@ -19,15 +19,14 @@ function handleGetMapRequest(req, res){
 	var layers = req.query.layers.split(',')
 
 	var map = new mapnik.Map(imgWidth, imgHeight);
-	var map2 = new mapnik.Map(imgWidth, imgHeight);
-	map2.loadSync(config.styleDir + 'world_merc/default.xml')
 
 	map.load(config.styleDir + layers[0] + '/' + styles[0] +".xml", function(err,map) {
 	    if (err) throw err;
 	    map.zoomAll();
 	    //map.extent = bbox;
-	    map.add_layer(map2.get_layer(0));
-	    console.log(map.layers())
+	    // console.log(map)
+	    // console.log(map.layers())
+	    map.srs = '+init=epsg:3857'
 	    var im = new mapnik.Image(imgWidth, imgHeight);
 	    map.render(im, function(err,im) {
 	        if (err) throw err;
